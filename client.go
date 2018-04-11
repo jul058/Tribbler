834d33fe4ebbcd488ff2012cf6bb48bd0f2ea9d3
Junxi Li <cse223b_jul058@vm173.sysnet.ucsd.edu>
2018-04-10 17:14:42 -0700

    finish w/o TODO

---
+++ client.go
@@ -0,0 +1,106 @@
+package triblab
+
+import (
+    "net"
+    "net/rpc"
+    "net/rpc/jsonrpc"
+    "trib"
+)
+
+type client struct {
+    ServerAddr string
+    conn *rpc.Client
+}
+var _ trib.Storage = new(client)
+
+func (self *client) Init() error {
+    // connect to server
+    conn, err := net.Dial("tcp", self.ServerAddr)
+
+    if err != nil {
+        return err
+    }
+
+    self.conn = rpc.NewClientWithCodec(jsonrpc.NewClientCodec(conn))
+    return nil
+}
+
+func (self *client) Get(key string, value *string) error {
+    // perform the call
+    err := self.conn.Call("Storage.Get", key, value)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (self *client) Set(kv *trib.KeyValue, succ *bool) error {
+    // perform the call
+    err := self.conn.Call("Storage.Set", kv, succ)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (self *client) Keys(p *trib.Pattern, list *trib.List) error {
+    // perform the call
+    err := self.conn.Call("Storage.Keys", p, list)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (self *client) ListGet(key string, list *trib.List) error {
+    // perform the call
+    err := self.conn.Call("Storage.ListGet", key, list)
+    if err != nil {
+        return nil
+    }
+
+    return nil
+}
+
+func (self *client) ListAppend(kv *trib.KeyValue, succ *bool) error {
+    // perform the call
+    err := self.conn.Call("Storage.ListAppend", kv, succ)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (self *client) ListRemove(kv *trib.KeyValue, n *int) error {
+    // perform the call
+    err := self.conn.Call("Storage.ListRemove", kv, n)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (self *client) ListKeys(p *trib.Pattern, list *trib.List) error {
+    // perform the call
+    err := self.conn.Call("Storage.ListKeys", p, list)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func (self *client) Clock(atLeast uint64, ret *uint64) error {
+    // perform the call
+    err := self.conn.Call("Storage.Clock", atLeast, ret)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
--- lab1.go
+++ lab1.go
@@ -1,15 +1,44 @@
 package triblab
 
 import (
-	"trib"
+    "net"
+    "net/rpc"
+    "net/rpc/jsonrpc"
+    "trib"
 )
 
 // Creates an RPC client that connects to addr.
 func NewClient(addr string) trib.Storage {
-	panic("todo")
+    conn := &client{ServerAddr: addr}
+    err := conn.Init()
+    if err != nil {
+        return nil
+    }
+
+    return conn
 }
 
 // Serve as a backend based on the given configuration
 func ServeBack(b *trib.BackConfig) error {
-	panic("todo")
+    server := rpc.NewServer()
+    server.RegisterName("Storage", b.Store)
+    server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
+
+    lis, err := net.Listen("tcp", b.Addr)
+    if err != nil {
+        b.Ready <- false
+        return err
+    }
+
+    b.Ready <- true
+    for {
+        conn, err := lis.Accept()
+        if err != nil {
+            b.Ready <- false
+            return err
+        }
+
+        go server.ServeCodec(jsonrpc.NewServerCodec(conn))
+    }
+    return nil
 }
