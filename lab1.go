26af52f6f0034af59e27ccfbf71dd1b4e821c27f
Junxi Li <cse223b_jul058@vm173.sysnet.ucsd.edu>
2018-04-18 10:45:25 -0700

    delete handle http

--- lab1.go
+++ lab1.go
@@ -9,14 +9,13 @@ import (
 
 // Creates an RPC client that connects to addr.
 func NewClient(addr string) trib.Storage {
-    return &client{ ServerAddr: addr, conn : nil}
+    return &client{ ServerAddr: addr, conn : nil }
 }
 
 // Serve as a backend based on the given configuration
 func ServeBack(b *trib.BackConfig) error {
     server := rpc.NewServer()
     server.RegisterName("Storage", b.Store)
-    server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
 
     lis, err := net.Listen("tcp", b.Addr)
     if err != nil {
@@ -28,6 +27,7 @@ func ServeBack(b *trib.BackConfig) error {
     if b.Ready != nil {
         b.Ready <- true
     }
+
     for {
         conn, err := lis.Accept()
         if err != nil {
